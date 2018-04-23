e0d18559e48037c575e979122598100f7efdbf67
stkane <stephenw.kane@gmail.com>
2018-04-23 17:20:41 -0400

    added autocomplete

---
+++ autocomplete.js
@@ -0,0 +1,39 @@
+// The autocomplete function will take in 
+// an input string and a dictionary array 
+// and return the values from the dictionary 
+// that start with the input string. If 
+// there are more than 5 matches, restrict 
+// your output to the first 5 results. If 
+// there are no matches, return an empty array.
+
+// Example:
+
+// autocomplete('ai', ['airplane','airport','apple','ball']) = ['airplane','airport']
+// For this kata, the dictionary will always 
+// be a valid array of strings. Please return 
+// all results in the order given in the 
+// dictionary, even if they're not always 
+// alphabetical. The search should NOT be 
+// case sensitive, but the case of the word 
+// should be preserved when it's returned.
+
+// For example, "Apple" and "airport" would 
+// both return for an input of 'a'. However, 
+// they should return as "Apple" and "airport" 
+// in their original cases.
+
+
+function autocomplete(input, dictionary) {
+  var matches = [];
+  let i = 0;
+  while(matches.length < 5 && dictionary.length > i) {
+    
+    let word = dictionary[i];
+    let lowerWord = word.toLowerCase();
+    
+    if(lowerWord.startsWith(input)) {matches.push(word)}
+    
+    i++;
+  }
+  return matches;
+}
