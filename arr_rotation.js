45ef2f00e295815273ffa8efd66e2cd1f5da94f1
stkane <stephenw.kane@gmail.com>
2018-07-30 13:55:46 -0600

    added array rotation answer

---
+++ arr_rotation.js
@@ -0,0 +1,17 @@
+// A left rotation operation on an array shifts 
+// each of the array's elements 1 unit to the left. 
+// For example, if 2 left rotations are performed 
+// on array [1, 2, 3, 4, 5], then the array would 
+// become [3, 4, 5, 1, 2].
+
+// Given an array 'a' of 'n' integers and a number, 'd', 
+// perform 'd' left rotations on the array. Return the 
+// updated array to be printed as a single line of 
+// space-separated integers.
+
+function rotLeft(a, d) {
+  for(let i = 0; i < d; i++){
+    a.push(a.shift());
+  }
+  return a;
+}
